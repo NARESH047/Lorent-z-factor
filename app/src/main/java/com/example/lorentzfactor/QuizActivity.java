1f418cdfbc8b3f31669af83209e15633af9abc76
naresh <nareshsivani2001@gmail.com>
2021-07-17 08:59:08 +0530

    Lorentz's Factor calculator

---
+++ app/src/main/java/com/example/lorentzfactor/QuizActivity.java
@@ -0,0 +1,18 @@
+package com.example.lorentzfactor;
+
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class QuizActivity extends AppCompatActivity {
+    Double Vel, c, C;
+    boolean MeterSec, FeetSec;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_checker);
+
+    }
+}
+
